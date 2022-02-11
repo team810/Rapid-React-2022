2d58288a355dd564336743af5f79c3b0f9e21ff5
Jack Patterson <jtyler03@optonline.net>
2022-02-11 12:32:36 -0500

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,44 +1,40 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  private double speed;
-  private boolean bool1;
+
   public CANSparkMax intake;
-  public Solenoid intakeSol;
+  public Solenoid intakeSolLeft, intakeSolRight;
 
   /** Creates a new Intake. */
   public Intake() {
-    intake = new CANSparkMax(Constants.INTAKE_MOTOR, MotorType.kBrushless);
-    intakeSol = new Solenoid(PneumaticsModuleType.REVPH, 9);
+    intake = new CANSparkMax(Constants.INTAKE, MotorType.kBrushless);
+    intakeSolLeft = new Solenoid(PneumaticsModuleType.REVPH, 9);
+    intakeSolRight = new Solenoid(PneumaticsModuleType.REVPH, 8);
+
   }
 
   @Override
   public void periodic() {
+
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Intake Speed", this.speed);
-    SmartDashboard.putBoolean("Solenoid on?", this.bool1);
   }
 
-  public void set(double speed) {
+  public void setIntake(boolean value, double speed){
+    intakeSolLeft.set(value);
+    intakeSolRight.set(value);
+    intake.setIdleMode(IdleMode.kBrake);
     intake.set(speed);
-    this.speed = speed;
   }
 
-  public void setSolenoid(boolean bool) {
-    intakeSol.set(bool);
-    this.bool1 = bool;
-  }
 }
