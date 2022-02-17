4ea40894fb72da86c4fc9676c45edf5839c8c2e6
Joseph DiMino <josephdimino05@gmail.com>
2022-02-17 18:03:45 -0500

    added gamepad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,6 +38,8 @@ public class RobotContainer {
   private Joystick LEFT = new Joystick(Constants.LEFT_JOYSTICK);
   private Joystick RIGHT = new Joystick(Constants.RIGHT_JOYSTICK);
 
+  private Joystick GAMEPAD = new Joystick(Constants.GP);
+
   private JoystickButton toggleLimelight,toggleLimelightCam, runShooter, runIntake, toggleIntake, runFeeder, raiseClimber, lowerClimber, ejectBall;
 
   /**
@@ -70,7 +72,7 @@ public class RobotContainer {
       ));
 
     toggleIntake = new JoystickButton(RIGHT, Constants.LEFT_BUTTON);
-    toggleIntake.whileHeld(new StartEndCommand(() -> m_intake.toggleIntakeSol(), () -> m_intake.toggleIntakeSol(), m_intake));
+    toggleIntake.toggleWhenPressed(new StartEndCommand(() -> m_intake.toggleIntakeSol(), () -> m_intake.toggleIntakeSol(), m_intake));
 
     runFeeder = new JoystickButton(LEFT, Constants.TRIGGER_BUTTON);
     runFeeder.whileHeld(new StartEndCommand(()-> m_feeder.runFeeder(.5), ()-> m_feeder.runFeeder(.5), m_feeder));
@@ -80,21 +82,21 @@ public class RobotContainer {
       new StartEndCommand(() -> m_feeder.runFeeder(-0.5), () -> m_feeder.runFeeder(0), m_feeder), 
       new StartEndCommand(() -> m_intake.runIntake(-0.5), () -> m_intake.runIntake(0), m_intake)
     ));
-    
+
     runShooter = new JoystickButton(RIGHT, Constants.MIDDLE_BUTTON);
     runShooter.toggleWhenPressed(new StartEndCommand(() -> m_shooter.runShooter(.5, .75), () -> m_shooter.runShooter(0, 0), m_shooter));
 
     //secondary
-    raiseClimber = new JoystickButton(RIGHT, Constants.LEFT_BUTTON);
+    raiseClimber = new JoystickButton(GAMEPAD, Constants.X);
     raiseClimber.whileHeld(new StartEndCommand(() -> m_climber.runClimber(0.5), () -> m_climber.runClimber(0), m_climber));
 
-    lowerClimber = new JoystickButton(RIGHT, Constants.RIGHT_BUTTON);
+    lowerClimber = new JoystickButton(GAMEPAD, Constants.B);
     lowerClimber.whileHeld(new StartEndCommand(() -> m_climber.runClimber(-0.5), () -> m_climber.runClimber(0), m_climber));
   
-    toggleLimelight = new JoystickButton(LEFT, Constants.LEFT_BUTTON);
+    toggleLimelight = new JoystickButton(GAMEPAD, Constants.LB);
     toggleLimelight.toggleWhenPressed(new StartEndCommand(()-> m_shooter.toggleLimelightLight(3), ()-> m_shooter.toggleLimelightLight(1), m_shooter));
 
-    toggleLimelightCam = new JoystickButton(LEFT, Constants.RIGHT_BUTTON);
+    toggleLimelightCam = new JoystickButton(GAMEPAD, Constants.RB);
     toggleLimelightCam.toggleWhenPressed(new StartEndCommand(()-> m_shooter.toggleLimelightCamMode(0), ()-> m_shooter.toggleLimelightLight(1), m_shooter));
   }
 
