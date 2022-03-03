1767ac48ed41c85dc1d1979693589dde76bcec4f
Aaquib <aaquibahm@gmail.com>
2022-03-03 15:09:44 -0500

    Commit

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -5,29 +5,38 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Feeder extends SubsystemBase {
 
   Spark feederMotor;
+  ColorSensorV3 colorSensor;
 
   /** Creates a new Indexer. */
   public Feeder() {
 
     feederMotor = new Spark(Constants.FEEDER);
-
+    colorSensor = new ColorSensorV3(Port.kOnboard);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Red", colorSensor.getRed());
+    SmartDashboard.putNumber("Green", colorSensor.getGreen());
+    SmartDashboard.putNumber("Blue", colorSensor.getBlue());
+    SmartDashboard.putNumber("Promity", colorSensor.getProximity());
   }
 
   public void runFeeder(double speed){
     feederMotor.set(speed);
   }
+
 }
